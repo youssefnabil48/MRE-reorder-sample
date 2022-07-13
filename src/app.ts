f5444e7e074cedc271284d8c261a6f884a363235
youssef nabil <youssef.nabil.mustafa@gmail.com>
2022-07-13 20:44:33 +0200

    make dumps file '

--- src/app.ts
+++ src/app.ts
@@ -51,6 +51,9 @@ export default class HelloWorld {
 		coster.onGrab('end', (user, data) => {
 			console.log('end ', coster.transform.app.toJSON())
 		})
+		/*coster.collider.onCollision('collision-enter', (actor: any) => {
+			console.log(actor)
+		})*/
 
 		const dna3dModel = await this.assets.loadGltf('dna.glb', 'box');
 		const dna = MRE.Actor.CreateFromPrefab(this.context, {
@@ -77,54 +80,6 @@ export default class HelloWorld {
 		dna.onGrab('end', (user, data) => {
 			console.log('end ', dna.transform.local.toJSON())
 		})
-
-		/*const circle = this.assets.createCylinderMesh('circle', 0.1, 0.4, 'y', 8);
-		const square = this.assets.createBoxMesh('square', 0.70, 0.2, 0.70);
-		const blackMaterial = this.assets.createMaterial("blackmat", {
-			color: MRE.Color3.Black()
-		});
-		const coster = MRE.Actor.Create(this.context, {
-			actor: {
-				name: 'coster',
-				appearance: { meshId: circle.id, enabled: true, materialId: blackMaterial.id },
-				grabbable: false,
-				collider: {
-					isTrigger: true,
-					geometry: {
-						shape: MRE.ColliderType.Sphere,
-						radius: 0.5
-					},
-					bounciness: 2,
-					enabled: true
-				},
-				rigidBody: {
-					useGravity: true,
-					collisionDetectionMode: MRE.CollisionDetectionMode.Continuous,
-					detectCollisions: true,
-					enabled: true,
-					mass: 1
-				},
-				subscriptions: ['transform', 'rigidbody'],
-				transform: { app: { rotation: { x: 0, y: 0.5, z: 0 } } }
-			},
-		})*/
-
-		// const ball = MRE.Actor.CreatePrimitive(this.assets, {
-		// 	definition: {
-		// 		dimensions: {x: 2, y: 2, z: 2},
-		// 		shape: MRE.PrimitiveShape.Sphere,
-		// 	},
-		// 	actor: {
-		// 		name: 'box',
-		// 		grabbable: true,
-		// 	},
-		// 	addCollider: true,
-		// })
-
-		// ball.subscribe('transform')
-		// ball.onGrab('end', () => {
-		// 	console.log(ball.transform.local.position.toJSON())
-		// })
 
 	}
 
